a06d28ebd39fff9130ab219030d1d500b61d52d9
yrung <yrungen@163.com>
2018-08-27 14:01:02 +0800

    Initial commit

---
+++ src/main/java/com/eirunye/springbootoperation/SpringbootoperationApplication.java
@@ -0,0 +1,12 @@
+package com.eirunye.springbootoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootoperationApplication.class, args);
+	}
+}
